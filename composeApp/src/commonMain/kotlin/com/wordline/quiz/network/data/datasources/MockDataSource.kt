b748a3454bff347057804264117b2341011fb12f
Emma Salot <emma.salot.etu@univ-lille.fr>
2025-02-14 11:36:48 +0100

    final commit

--- composeApp/src/commonMain/kotlin/com/wordline/quiz/network/data/datasources/MockDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/wordline/quiz/network/data/datasources/MockDataSource.kt
@@ -7,24 +7,24 @@ class MockDataSource {
 
     fun generateDummyQuestionsList():List<Question>{
         return listOf(
-//            Question(
-//                1,
-//                "Android is a great platform ?",
-//                1,
-//                listOf(
-//                    Answer( 1,"YES"),
-//                    Answer(2,"NO")
-//                )
-//            ),
-//            Question(
-//                1,
-//                "Android is a bad platform ?",
-//                2,
-//                listOf(
-//                    Answer( 1,"YES"),
-//                    Answer(2,"NO")
-//                )
-//            )
+            Question(
+                1,
+                "Android is a great platform ?",
+                1,
+                listOf(
+                    Answer( 1,"YES"),
+                    Answer(2,"NO")
+                )
+            ),
+            Question(
+                1,
+                "Android is a bad platform ?",
+                2,
+                listOf(
+                    Answer( 1,"YES"),
+                    Answer(2,"NO")
+                )
+            )
         )
 
     }
